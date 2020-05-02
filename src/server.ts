7d752e4a9eb79bb52ebae84735fe81f667421731
Christian Santos <christian.santos@ibti.org.br>
2020-05-01 22:45:59 -0300

    servindo arquivos estaticos

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,17 @@
 import 'reflect-metadata';
-import './database';
 import express from 'express';
+
+import './database';
 import routes from './routes';
+import uploadConfig from './config/upload';
 
 const app = express();
 
 app.use(express.json());
 
 app.use(routes);
+
+app.use('/files', express.static(uploadConfig.directory));
 
 app.listen(3333, () => {
   console.log('listening on port :3333 🎈🎈🎈🎈');
