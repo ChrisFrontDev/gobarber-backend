79e066586d3ca69f0a5553d2b530299a6b2b147e
Christian Santos <christian.pinho@outlook.com>
2020-08-06 04:30:58 -0300

    salvando tokens no banco

---
+++ src/modules/users/infra/http/controllers/ResetPasswordController.ts
@@ -0,0 +1,16 @@
+// only index,show,create,update,delete
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import ResetPasswordService from '@modules/users/services/ResetPasswordService';
+
+export default class ResetPasswordController {
+  async create(request: Request, response: Response): Promise<Response> {
+    const { password, token } = request.body;
+
+    const sendForgotPasswordEmail = container.resolve(ResetPasswordService);
+
+    await sendForgotPasswordEmail.execute({ password, token });
+
+    return response.status(204).json();
+  }
+}
